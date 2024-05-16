584cdce338b47d9443607dc44ea1cd88e0ed7737
Sumant Kumbar <69085203+SumanthKumbar@users.noreply.github.com>
2024-05-16 16:27:00 +0530

    Add factorial function

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
 console.log("this is from main.js file");
 
+function factorial(n) {
+	if (n == 0 || n == 1) {
+		return 1;
+	}
+	return n * factorial(n-1);
+}
+
 function sum(a, b) {
 	return a + b
 }
